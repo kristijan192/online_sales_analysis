759d6e3dc68a80315250bc650ecdc19660dd1f5d
Kristijan T <ktodorovic12@hotmail.com>
2025-05-22 20:48:43 +0200

    Dodata metoda

--- product_manager.py
+++ product_manager.py
@@ -20,3 +20,9 @@ class ProductManager:
             print(f"Total value of all products: ${total}")
             return total
       
+      def remove_product(self, product_name):
+       for product in self.products:
+        if product.name == product_name:
+            self.products.remove(product)
+            return True
+       return False
